ea8c407bed140969b11a4894f69b2cfa8bebd968
thetechsavage96 <thetechsavage96@gmail.com>
2025-06-18 11:22:30 +0100

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -7,7 +7,7 @@
 #
 # All rights reserved.
 
-from pyrogram import Client 
+from pyrogram import Client
 from bot import Bot
 from config import *
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
@@ -18,8 +18,12 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
 
     if data == "help":
+        # <<< MODIFIED: Fetch custom help text from DB >>>
+        help_text_setting = await db.get_setting("help_text")
+        text = help_text_setting['value'] if help_text_setting else HELP_TXT
+
         await query.message.edit_text(
-            text=HELP_TXT.format(first=query.from_user.first_name),
+            text=text.format(first=query.from_user.first_name),
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup([
                 [InlineKeyboardButton('ʜᴏᴍᴇ', callback_data='start'),
@@ -28,8 +32,12 @@ async def cb_handler(client: Bot, query: CallbackQuery):
         )
 
     elif data == "about":
+        # <<< MODIFIED: Fetch custom about text from DB >>>
+        about_text_setting = await db.get_setting("about_text")
+        text = about_text_setting['value'] if about_text_setting else ABOUT_TXT
+
         await query.message.edit_text(
-            text=ABOUT_TXT.format(first=query.from_user.first_name),
+            text=text.format(first=query.from_user.first_name),
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup([
                 [InlineKeyboardButton('ʜᴏᴍᴇ', callback_data='start'),
@@ -38,13 +46,38 @@ async def cb_handler(client: Bot, query: CallbackQuery):
         )
 
     elif data == "start":
-        await query.message.edit_text(
-            text=START_MSG.format(first=query.from_user.first_name),
-            disable_web_page_preview=True,
-            reply_markup=InlineKeyboardMarkup([
+        # This part now fetches its content dynamically from start.py,
+        # but we need to reconstruct the buttons here for the back button.
+        start_pic_setting = await db.get_setting("start_pic")
+        start_text_setting = await db.get_setting("start_text")
+        channel_button_setting = await db.get_setting("channel_button")
+
+        pic = start_pic_setting['value'] if start_pic_setting else START_PIC
+        text = start_text_setting['value'] if start_text_setting else START_MSG
+
+        buttons = []
+        if channel_button_setting:
+            btn_text = channel_button_setting['value']['text']
+            btn_url = channel_button_setting['value']['url']
+            buttons.append([InlineKeyboardButton(btn_text, url=btn_url)])
+        else:
+            buttons.append([InlineKeyboardButton("• THETECHSAVAGE CHANNELS •", url="https://t.me/TheTechSavageTelegram")])
+        
+        buttons.append([
                 [InlineKeyboardButton("ʜᴇʟᴘ", callback_data='help'),
                  InlineKeyboardButton("ᴀʙᴏᴜᴛ", callback_data='about')]
             ])
+
+        await query.message.edit_text(
+            text=text.format(
+                first=query.from_user.first_name,
+                last=query.from_user.last_name,
+                username=f"@{query.from_user.username}" if query.from_user.username else "N/A",
+                mention=query.from_user.mention,
+                id=query.from_user.id
+            ),
+            disable_web_page_preview=True,
+            reply_markup=InlineKeyboardMarkup(buttons)
         )
 
     elif data == "close":
@@ -53,7 +86,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
             await query.message.reply_to_message.delete()
         except:
             pass
-
+            
     elif data.startswith("rfs_ch_"):
         cid = int(data.split("_")[2])
         try:
@@ -80,7 +113,6 @@ async def cb_handler(client: Bot, query: CallbackQuery):
         await db.set_channel_mode(cid, mode)
         await query.answer(f"Force-Sub set to {'ON' if mode == 'on' else 'OFF'}")
 
-        # Refresh the same channel's mode view
         chat = await client.get_chat(cid)
         status = "🟢 ON" if mode == "on" else "🔴 OFF"
         new_mode = "off" if mode == "on" else "on"
